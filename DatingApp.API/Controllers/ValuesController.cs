d9bb23e7021f00ce9a95e52c7eafa6b299655f40
AVRMaster <periss92@hotmail.com>
2020-03-21 19:13:31 +0200

    first commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using DatingApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : Controller
+    {
+        private readonly DataContext context;
+
+        public ValuesController(DataContext _context)
+        {
+            context = _context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await context.Values.FirstOrDefaultAsync(x => x.ID == id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        [HttpPost("{id}")]
+        public void Post(int id, [FromBody] string value)
+        {
+
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,7 @@
+namespace DatingApp.API.Models{
+    public class Value
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
